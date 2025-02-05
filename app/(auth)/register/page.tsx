44dd47dc8f6e627351d8b5c3f46cdd58d9bccebb
feranmiola <osunjuyigbeiyin@gmail.com>
2025-02-05 15:05:42 +0100

    Improve register page layout and text alignment

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -6,9 +6,9 @@ import SellerForm from './SellerForm';
 export default function Page() {
   return (
     <main className="space-y-8 py-12">
-      <Container className="space-y-2 text-center">
+      <Container className="space-y-2 text-center w-full flex items-center justify-center flex-col">
         <h2 className="text-4xl">Register</h2>
-        <p>
+        <p className='max-w-[836px] text-center'>
           Are you a buyer or a seller? Kindly provide accurate details to ensure
           a smooth and secure experience on Tokunbo.
         </p>
