c8a77af2a51964fb4fed70bcc0ff3f4944120122
feranmiola <osunjuyigbeiyin@gmail.com>
2025-02-21 11:59:34 +0100

    Add React Query and Axios for state management and HTTP requests

---
+++ app/Providers.tsx
@@ -0,0 +1,17 @@
+// app/providers.tsx
+'use client';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
+import NextTopLoader from 'nextjs-toploader';
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextTopLoader showSpinner={false} />
+      <AntdRegistry>{children}</AntdRegistry>
+    </QueryClientProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from 'next';
 import { Montserrat, Milonga } from 'next/font/google';
-import NextTopLoader from 'nextjs-toploader';
+
 import './globals.css';
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
-import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { Providers } from './Providers';
 
 export const milonga = Milonga({ subsets: ['latin'], weight: ['400'] });
 const montserrat = Montserrat({ subsets: ['latin'] });
@@ -22,14 +22,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
-        <NextTopLoader showSpinner={false} />
-        <AntdRegistry>
+        <Providers>
           <section className="grid min-h-screen grid-rows-[auto_1fr_auto]">
             <Header />
             {children}
             <Footer />
           </section>
-        </AntdRegistry>
+        </Providers>
       </body>
     </html>
   );
