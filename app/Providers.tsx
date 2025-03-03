7a639d75ae4d51274e22e634fcec262d23329d90
feranmiola <osunjuyigbeiyin@gmail.com>
2025-03-03 10:02:55 +0100

    Wrap app providers with AuthContext for global authentication state

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -5,14 +5,17 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import NextTopLoader from 'nextjs-toploader';
 import { Toaster } from 'sonner';
+import { AuthProvider } from './Context/AuthContext';
 const queryClient = new QueryClient();
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NextTopLoader showSpinner={false} />
-      <AntdRegistry>{children}</AntdRegistry>
-      <Toaster />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <NextTopLoader showSpinner={false} />
+        <AntdRegistry>{children}</AntdRegistry>
+        <Toaster />
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
